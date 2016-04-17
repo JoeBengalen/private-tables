9bcd92e9a75f5630fdd6d3779ea3b1d96fb9ce9b
Martijn Wennink <joebengalen@gmail.com>
2016-04-17 18:37:51 +0200

    Database migration and seed

---
+++ db/migrations/20160330210418_initial.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    /**
+     * Change to the initial database state.
+     */
+    public function change()
+    {
+        $this->table('table')
+                ->addColumn('name', 'string', ['length' => 255])
+                ->addIndex('name', ['unique' => true])
+                ->create();
+
+        $this->table('field')
+                ->addColumn('tableId', 'integer')
+                ->addColumn('name', 'string', ['length' => 255])
+                ->addColumn('type', 'string', ['length' => 15])
+                ->addColumn('length', 'integer', ['null' => true])
+                ->addColumn('allowNull', 'boolean', ['default' => false])
+                ->addColumn('default', 'string', ['length' => 255, 'null' => true])
+                ->addColumn('comment', 'string', ['length' => 255, 'null' => true])
+                ->addColumn('isPrimaryKey', 'boolean')
+                ->addColumn('autoIncrement', 'boolean')
+                ->addForeignKey('tableId', 'table', 'id')
+                ->create();
+
+        $this->table('index')
+                ->addColumn('fieldId', 'integer')
+                ->addColumn('name', 'string', ['length' => 255])
+                ->addColumn('unique', 'boolean')
+                ->addForeignKey('tableId', 'field', 'id')
+                ->create();
+
+        $this->table('foreignKey')
+                ->addColumn('fieldId', 'integer')
+                ->addColumn('referencedFieldId', 'integer')
+                ->addColumn('onUpdate', 'string', ['length' => 10])
+                ->addColumn('onDelete', 'string', ['length' => 10])
+                ->addForeignKey('tableId', 'field', 'id')
+                ->addForeignKey('referencedFieldId', 'field', 'id')
+                ->create();
+    }
+}
---
+++ db/seeds/TestDummyData.php
@@ -0,0 +1,44 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class TestDummyData extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $this->insert('table', [
+            [
+                'id' => 1,
+                'name' => 'table1',
+            ],
+            [
+                'id' => 2,
+                'name' => 'table2',
+            ]
+        ]);
+
+        $this->insert('field', [
+            [
+                'id' => 1,
+                'tableId' => 1,
+                'name' => 'id',
+                'type' => 'INTEGER',
+            ],
+            [
+                'id' => 2,
+                'tableId' => 1,
+                'name' => 'fieldname',
+                'type' => 'STRING',
+                'length' => 255,
+            ]
+        ]);
+    }
+}
